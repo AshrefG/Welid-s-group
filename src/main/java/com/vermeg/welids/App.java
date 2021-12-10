70ae3e4ff8869018d40a5b86d382f2027bbf1a23
AshrefG <ashref.gammoudi@gmail.com>
2021-12-10 14:47:16 +0100

    copie 0

---
+++ src/main/java/com/vermeg/welids/App.java
@@ -0,0 +1,13 @@
+package com.vermeg.welids;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
