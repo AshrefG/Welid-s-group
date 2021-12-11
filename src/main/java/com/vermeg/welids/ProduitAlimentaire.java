af7b6dc2e22a4b8d6ed88233ce0f2097c148b340
Hammami Amir <hammamiamir1999@gmail.com>
2021-12-11 14:58:04 +0100

    getter & setter added

--- src/main/java/com/vermeg/welids/ProduitAlimentaire.java
+++ src/main/java/com/vermeg/welids/ProduitAlimentaire.java
@@ -25,4 +25,43 @@ public class ProduitAlimentaire {
 	         if(prix>0)
 	        this.prix=prix;
 	    }
+	    public int getIdentifiant(){
+	        return identifiant;
+	    }
+	    public void setIdentifiant(int identifiant){
+	        this.identifiant=identifiant;
+	    }
+
+	    public String getLibelle() {
+	        return libelle;
+	    }
+
+	    public void setLibelle(String libelle) {
+	        this.libelle = libelle;
+	    }
+
+	    public String getMarque() {
+	        return marque;
+	    }
+
+	    public void setMarque(String marque) {
+	        this.marque = marque;
+	    }
+
+	    public Date getDate_expiration() {
+	        return date_expiration;
+	    }
+
+	    public void setDate_expiration(Date date_expiration) {
+	        this.date_expiration = date_expiration;
+	    }
+	    
+	    public void setPrix(float prix){
+	        if(prix>0)
+	            this.prix=prix;
+	    }
+
+	    public float getPrix() {
+	        return prix;
+	    }
 }
