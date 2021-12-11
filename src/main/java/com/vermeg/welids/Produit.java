a6fb817b451b92c59911170f369d55cbf5cdafcb
taher toumi <taher.toumi@esprit.tn>
2021-12-11 14:44:03 +0100

    la creation de des gettres and setters

--- src/main/java/com/vermeg/welids/Produit.java
+++ src/main/java/com/vermeg/welids/Produit.java
@@ -28,7 +28,55 @@ public class Produit {
 	        	 
 	        this.prix=prix;
 	         }
+	    	}
+	    // les getters and setters  
+	    public int getIdentifiant(){
+	        return identifiant;
+	    }
+	    public void setIdentifiant(int identifiant){
+	        this.identifiant=identifiant;
+	    }
 
-}
+	    public String getLibelle() {
+	        return libelle;
+	    }
+
+	    public void setLibelle(String libelle) {
+	        this.libelle = libelle;
+	    }
+
+	    public String getMarque() {
+	        return marque;
+	    }
+
+	    public void setMarque(String marque) {
+	        this.marque = marque;
+	    }
+
+	    public Date getDate_expiration() {
+	        return date_expiration;
+	    }
+
+	    public void setDate_expiration(Date date_expiration) {
+	        this.date_expiration = date_expiration;
+	    }
+	    
+	    public void setPrix(float prix){
+	        if(prix>0)
+	            this.prix=prix;
+	    }
+
+	    public float getPrix() {
+	        return prix;
+	    }
+	    
+	    public void afficher(){
+	        System.out.println("ID :"+identifiant+" , Libelle : "
+	                + ""+libelle+" ,Marque : "+marque+" , prix : "+prix);
+	    } 
+	    
+	    
+	    
+	    
 	    
 }
