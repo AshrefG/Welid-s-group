be45c3ff270240a99226006fbfed35e6a56de941
Firas Krifa <firaskrifa02@gmail.com>
2021-12-11 11:34:53 +0100

    copie 0

---
+++ src/main/java/com/vermeg/Magasin.java
@@ -0,0 +1,12 @@
+package com.vermeg;
+
+public class Magasin {
+	private int id;
+	   private String nom;
+	   private String adresse;
+	   public  static final int CAPACITE=50;
+	   private Produit[] prods;//=new Produit[CAPACITE];
+	   public int cmp=0;
+	   public static int totale=0;
+
+}
