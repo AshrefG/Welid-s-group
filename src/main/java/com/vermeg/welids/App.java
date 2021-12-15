8eab6124c9b01d67f135e04feeff9bf2bcfce565
fadhel ben hamida <fadhel.bhamida@gmail.com>
2021-12-15 19:41:16 +0100

    new commit

--- src/main/java/com/vermeg/welids/App.java
+++ src/main/java/com/vermeg/welids/App.java
@@ -1,18 +1,13 @@
 package com.vermeg.welids;
 // another commit
 // 15/12/2021
-
+///message message
 //fadhel's commit here
 public class App 
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
     	Employe e5 = new Employe(5,"fadhel","bardo",45);
     	
-=======
-    	// test 1 
-        System.out.println( "Hello World!" );
->>>>>>> 1ca35bab9d2758c764b73537280037396c79299d
     }
 }
