f173a4f41bfaed0674ca3d9430b03052835d2160
AshrefG <ashref.gammoudi@gmail.com>
2021-12-15 22:31:58 +0100

    last commit

--- src/main/java/com/vermeg/welids/Vendeur.java
+++ src/main/java/com/vermeg/welids/Vendeur.java
@@ -11,4 +11,26 @@ public class Vendeur extends Employe{
         super(id, nom, adresse, nbr_heure);
         this.tauxDeVente = tauxDeVente;
     }
+    public float getTauxDeVente() {
+        return tauxDeVente;
+    }
+
+    public void setTauxDeVente(float tauxDeVente) {
+        this.tauxDeVente = tauxDeVente;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+"Vendeur{" + "tauxDeVente=" + tauxDeVente + '}';
+    }
+
+    @Override
+    public float calculerSalaire() {
+        
+        return 450*tauxDeVente;
+    }
+    
+    
+    
+    
 }
